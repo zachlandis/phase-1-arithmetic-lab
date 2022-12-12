7cda608de82af0d8687bfaba9b5c1c60c7a67753
Zach <zachlandis91@gmail.com>
2022-12-12 13:18:25 -0700

    commit tree 70a6b17d336408447926ef5418b372d58835101a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 16
+const num4 = 6
+
+const mod = 16 % 6;
+
+const max = Math.max(1, 5, 10, 15, 20)
